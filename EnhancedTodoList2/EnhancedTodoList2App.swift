06bd92639a9e6dbd19e07a08de36f28f194d3499
Melody Li <zli@lcs.on.ca>
2024-11-12 11:20:39 -0500

    Initial Commit

---
+++ EnhancedTodoList2/EnhancedTodoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoList2App.swift
+//  EnhancedTodoList2
+//
+//  Created by 利梓萌 on 2024-11-12.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
